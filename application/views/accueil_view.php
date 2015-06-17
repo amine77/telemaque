b3199425417fd652aa2da58ac1f542eaf23cd949
LinkFox <LinkFox@192.168.4.8>
2015-06-17 14:37:10 +0200

    yoni -> update view

--- application/views/accueil_view.php
+++ application/views/accueil_view.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
-
+<h1>Jonathan arette la fumette</h1>
 <?php include 'templates/header.php';?>
 
 <h3>Salut :<?=$nom?> <?=$prenom?></h3>
