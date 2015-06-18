ba53d96c6964bdbd27d2811398103346a444e94a
amine <amine@192.168.4.6>
2015-06-18 16:55:16 +0200

    new commit

--- application/controllers/Front.php
+++ application/controllers/Front.php
@@ -2,10 +2,11 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Front extends CI_Controller {
 
     public function index() {
-        $this->load->view('front/welcome_message');
+
+        $this->load->view('front/home');
     }
 
     
