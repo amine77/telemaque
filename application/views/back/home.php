6ea3210e4e5efc5e4d66927693081772b271b267
amine <amine@192.168.4.6>
2015-06-19 16:41:18 +0200

    Merge origin/master
    Conflicts:
    	application/controllers/Admin.php
    	application/views/back/home.php
    	application/views/back/login.php

--- application/views/back/home.php
+++ application/views/back/home.php
@@ -1,6 +1,12 @@
 <div id="bloc_contenu">
     <h1>Tableau de bord</h1>
 
+
+
+<p class="text-right"><a href="<?php echo site_url('admin/logout');?>" type="button" class="btn btn-primary">Déconnexion</a></p>
+<h1>Bienvenue <?=$_SESSION['login'] ?> , vous êtes sur l'espace Administration</h1>
+
+
     <div id="bloc_recap_visiteurs">
         Visiteurs inscrits : 	XXXXXX
         <br>
@@ -14,3 +20,4 @@
         </tr>
     </table>
 </div>
+
