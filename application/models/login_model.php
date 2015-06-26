142c2c9fb1142cafd88f78ff06efafbed95d9733
dev <dev@192.168.4.38>
2015-06-26 13:16:38 +0200

    Ajout des fonctions sigup_user et mail_exists relatives à l'inscription des utilisateurs

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -15,4 +15,33 @@ class login_model extends CI_Model
           $query = $this->db->query($sql);
           return $query->num_rows();
      }
+     function signup_user($user_name, $user_surname, $login, $password, $born_at, $phone, $mobile, $mail)
+    {
+
+        $data = array(
+            'user_name' => $user_name,
+            'user_surname' => $user_surname,
+            'login' => $login,
+            'password' => $password,
+            'born_at' => $born_at,
+            'phone' => $phone,
+            'mobile' => $mobile,
+            'mail' => $mail,
+            'role_id'=>3   //par défaut les utilisateurs qui se connectent auront ROLE_USER
+        );
+
+        $this->db->insert('users', $data);
+        return ($this->db->affected_rows() != 1) ? false : true; //pour vérifier si l'insertion s'est bien déroulée.
+    }
+
+    function mail_exists($key)
+    {
+        $this->db->where('mail', $key);
+        $query = $this->db->get('users');
+        if ($query->num_rows() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }?>
