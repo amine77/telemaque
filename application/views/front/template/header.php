97deb98a931fc765b359921e571d5e9d149c64ff
dev <dev@192.168.4.38>
2015-06-26 13:11:07 +0200

    ajout de deux liens dans le header relatives à la connexion et à l'inscription des utilisateurs

--- application/views/front/template/header.php
+++ application/views/front/template/header.php
@@ -2,12 +2,18 @@
   
     <div>
         <img src ="http://www.google.fr/url?source=imglanding&ct=img&q=http://mon-logo-discount.com/logotek/577-822-proditem/logo-encart-entreprise.jpg&sa=X&ei=xOyDVaCKMojXyQPFsL8w&ved=0CAkQ8wc4FA&usg=AFQjCNEnLf2LDDM1XeZIA2p-wIH7Nf_ntQ" alt="logo entreprise" width="100" height="100"/>
-        <a href="#" style="float:right;margin-left:5px;"> Inscription</a>
-        <a href="#" style="float: right">Connexion</a>
         
+        <div style="float: right">
+            
+            <?php if($this->session->userdata('login')){
+     echo 'Bonjour <strong>'.$_SESSION['login'].'</strong>&nbsp;&nbsp;&nbsp;<a href="'.site_url('admin/logout').'">Déconnexion</a>';
+            }else{
+                echo ' <a href="'.site_url('admin').'">Connexion</a>&nbsp;&nbsp;&nbsp;<a href="'.site_url('inscription').'">Inscription</a>';
+            }
+            ?>
+           
+        </div>
     </div>
-
-
     <ul id="menu">
         <li> <?php echo '<a href="'.  base_url().'">Accueil</a>' ?></li>
         <li><a href="#">rub1</a></li>
