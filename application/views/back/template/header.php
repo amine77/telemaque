e961ce7ce0d0fcadbab7ed60ac2abaa5e18211aa
amine <amine@192.168.4.6>
2015-07-17 10:28:16 +0200

    Ajout de target _blank sur le lien "Allervers le Frontoffice"

--- application/views/back/template/header.php
+++ application/views/back/template/header.php
@@ -5,7 +5,7 @@
 
     
     <div id="bloc_session">
-        <a href="<?php echo base_url(); ?>">Aller vers le Frontoffice</a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a target="_blank" href="<?php echo base_url(); ?>">Aller vers le Frontoffice</a>&nbsp;&nbsp;&nbsp;&nbsp;
         Bonjour <?= ((isset($_SESSION['login'])==true )?$_SESSION['login']:'') ?><br>
         <a href="<?php echo site_url('admin/logout');?>">Déconnexion</a>
     </div>
