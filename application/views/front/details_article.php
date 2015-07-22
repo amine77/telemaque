4f978441835f5b8fd5613c5e84528e4b5b1d58ac
jvasseur <jvasseur@192.168.4.5>
2015-07-22 16:59:11 +0200

    Page du détail des articles en cours

--- application/views/front/details_article.php
+++ application/views/front/details_article.php
@@ -1,95 +1,101 @@
-<div class="center">
+<div id="bloc_contenu">
     <?php
     defined('BASEPATH') OR exit('No direct script access allowed');
 
 // Affiche les articles
     ?>
-    <div style='min-height:150px;border:1px solid grey;margin-bottom:15px'>
-        <h3><?= $article->article_label ?></h3>
+
+    <h3><?= $article->article_label ?></h3>
+
+
+    <div id="bloc_details_article">
         <div id="image_article">
             <?= $article->img['imsrc'] ?>
         </div>
-        <table id="caracteristiques" width="300px">
+
+        <table id="caracteristiques">
             <caption><h4><b>Caracteristiques generaux </b></h4></caption>
-            
             <tbody>
                 <?php
                 for ($i = 0; $i < count($article->spec); $i++) {
                     $spec = $article->spec[$i];
                     echo "<tr>
-                             <td> $spec->specification_label</td>
-                             <td> $spec->specification_value</td>
+                             <td class='col_1'> $spec->specification_label : </td>
+                             <td class='col_2'> $spec->specification_value</td>
                          </tr>";
                 }
                 ?>
             </tbody>
         </table>
+
+        <div class="clear"></div>
+
         <div id="description">
             <h4><b>Description</b></h4>
             <?= $article->description ?>
         </div>
-
     </div>
+
+
+
+
     <?php
     if (count($vendeurs_articles) > 0) {
         foreach ($vendeurs_articles as $row) :
             ?>
-            <div class="vendeurs">
 
+
+            <div class="vendeurs">
+                <h3>Exemplaires en vente</h3>
                 <div>
                     <div class="title">
                         <h4>  <?= ucfirst($row->title) ?> </h4>
                     </div>
-                    <div class="image_mini">
-                        <?= $row->img ?>
-                    </div>
-                    <div class="carac">
-                        <strong> Caracteristiques :</strong> <br/>
-                        <table width="300px">
-                      
-                            <tbody>
-                                <?php
-                                    for ($i = 0; $i < count($row->spec); $i++) {
-                                        $spec = $row->spec[$i];
-                                        echo "<tr>
-                                                <td> $spec->specification_label</td>
-                                                <td> $spec->specification_value</td>
-                                              </tr>";
-                                    }
-                                ?>
-                            </tbody>
-                        </table>  
-                        <br/>
-                    </div>
-                    <a href="<?= base_url() . 'usr/' . $row->user_id ?>">
-                        <strong><?= $row->user_name . ' ' . $row->user_surname; ?>
-                        </strong>
-                    </a>
-                    <a class="btn btn-primary" href="<?= base_url() . 'articles/' . $row->article_id . '/' . $row->user_id ?>">Voir le produit</a>
-                    <div class="quantity">
 
-                        <?php
-                        echo '<span style="font-size:15px;">' . $row->quantity . '</span> exemplaire';
-                        echo (intval($row->quantity) > 1) ? 's' : ''
-                        ?>
-                    </div>
-                    <div id="prix">
-                        <h3><?= $row->price ?>€</h3>
-                    </div>
+                    <table id="tableau_liste_exemplaires">
+                        <tr>
+                            <td class="image_mini"><?= $row->img ?></td>
+
+                            <td class="carac">
+                                <?php
+                                for ($i = 0; $i < count($row->spec); $i++) {
+                                    $spec = $row->spec[$i];
+                                    echo $spec->specification_label . " : " . $spec->specification_value;
+                                }
+                                ?>
+                            </td>
+                            <td>
+                                Mis en vente par :
+                                <a href="<?= base_url() . 'usr/' . $row->user_id ?>">
+                                    <strong><?= $row->user_name . ' ' . $row->user_surname; ?>
+                                    </strong>
+                                </a>
+                            </td>
+                            <td>
+                                <?php
+                                echo '<span style="font-size:15px;">' . $row->quantity . '</span> exemplaire';
+                                echo (intval($row->quantity) > 1) ? 's' : ''
+                                ?>
+                            </td>
+                            <td>
+                                <h3><?= $row->price ?>€</h3>
+                            </td>
+                            <td>
+                                <a class="btn_base" href="<?= base_url() . 'articles/' . $row->article_id . '/' . $row->user_id ?>">Voir le produit</a>
+                            </td>
+                        </tr>
+                    </table>
+
+
                 </div>
 
             </div>
-
-
-
-
-
-            <?php
-        endforeach;
-    } else {
-        echo 'Pas de vendeur pour cette article';
-    }
-    ?>
-
-
+            
+                    <?php
+                endforeach;
+} else {
+    echo 'Pas de vendeur pour cette article';
+}
+?>
+    
 </div>
