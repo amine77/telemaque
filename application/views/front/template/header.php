c0accfad978a74fb21c0b32b1be68b8cc72dbc96
amine <amine@192.168.4.6>
2015-07-23 16:39:53 +0200

    Transformer les categories en menu du front

--- application/views/front/template/header.php
+++ application/views/front/template/header.php
@@ -10,49 +10,42 @@
             ?>
         </div>
     </div>
-    
+
     <div id="bloc_head_centre">
         <div id="bloc_logo">
-            
+
         </div>
     </div>
-    
+    <?php
+            
+            $parents =array();
+            var_dump($categories);
+            foreach ($categories as $categorie) {
+                if($categorie['parent_category'] == '0'){
+                    $parents[]=$categorie['category'];
+                }else{
+                    $parents[$categorie['parent_category']][]=$categorie['category'];
+                }
+                
+            }
+            var_dump($parents);
+            ?>
     <nav>
-         <ul>
+        <ul>
             <li> <?php echo '<a href="' . base_url() . '">Accueil</a>' ?></li>
             <?php
-            var_dump($categories);
-            for($i=0;$i<count($categories);$i++){
-                $sousCat=false;
-                $count = 0;
-                if($categories[$i]['parent_category']==0){
-                    echo "<li>".$categories[$i]['category'];
-                }
-                else{
-                    $sousCat = true;
-                    if ($count==0)
-                        echo "<ul>";
-                     echo "<li>".$categories[$i]['category']."</li>";
-                  
-                }
-                if($sousCat){
-                    echo "</ul>";
-                }
-                echo "</li>";
-            }
+//                        foreach ($parents as $parent => $enfant) {
+//                            echo '<li><a href="#">'.$parent .'</a></li>';
+//                        }
             ?>
-            <li><a href="#">rub1</a></li>
-            <li><a href="#">rub2</a></li>
-            <li><a href="#">rub3</a></li>
-            <li><a href="#">rub4</a></li>
         </ul>
-        
+
         <div id="autres">
             <div id="form_search">
-                 <?php echo form_open('search'); ?>
-	<input type="search" name="recherche" id="search"/>
-	<input type="submit" value="Valider">
-<?php echo form_close(); ?>
+                <?php echo form_open('search'); ?>
+                <input type="search" name="recherche" id="search"/>
+                <input type="submit" value="Valider">
+                <?php echo form_close(); ?>
             </div>
             <div id="panier">
                 <?php echo '<a href="' . base_url() . 'panier">Panier</a>(<span>' . $nb_article . '</span>)'; ?>
@@ -60,6 +53,6 @@
         </div>
         <div class="clear"></div>
     </nav>
-    
-    
+
+
 </header>
