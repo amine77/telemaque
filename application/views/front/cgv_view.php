1d911b87540bff6d5fb0b68eda9f65dd31042c43
dev <dev@192.168.4.38>
2015-08-05 16:45:48 +0200

    Finalisation des pages de( mentions légales + CGV )

--- application/views/front/cgv_view.php
+++ application/views/front/cgv_view.php
@@ -1,12 +1,16 @@
-<!-- test depuis mon travail -->
+
 <div id="bloc_contenu">
     <?php
         defined('BASEPATH') OR exit('No direct script access allowed');
         
 
     ?>
+    <?php if(isset($site) && $site['cgv'] !='') ?>
+    <br><h3>Conditions générales de ventes</h3>
     <div class="well">
+        
         <?=  $site['cgv'] ?>
     </div>
+    <?  } ?>
 
 </div>
--- application/views/front/legal_notice_view.php
+++ application/views/front/legal_notice_view.php
@@ -1,12 +1,16 @@
-<!-- test depuis mon travail -->
+
 <div id="bloc_contenu">
     <?php
         defined('BASEPATH') OR exit('No direct script access allowed');
         
 
     ?>
+    <?php if(isset($site) && $site['legal_notice'] !='') ?>
+    <br><h3>Mentions légales</h3>
     <div class="well">
+        
         <?=  $site['legal_notice'] ?>
     </div>
+    <?  } ?>
 
 </div>
