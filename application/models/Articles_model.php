c3802d8238d69fcb048170378fcdb4b5a14e3d19
amine <amine@192.168.1.75>
2015-09-09 22:33:19 +0200

    Supprimer les doublons du résultat de recherche lorsque le terme recherché correspond au titre de l'article et au mot clé

--- application/models/Articles_model.php
+++ application/models/Articles_model.php
@@ -163,7 +163,7 @@ class Articles_model extends CI_Model {
 
     //cette fonction effectue une recherche sur les labels des articles ainsi que leurs mots clés associés
     public function search($key) {
-        $sql = "SELECT  articles.article_id, article_label, image_path
+        $sql = "SELECT DISTINCT articles.article_id, article_label, image_path
             FROM images
            RIGHT JOIN articles ON articles.image_id = images.image_id
            RIGHT JOIN tags_articles ON tags_articles.article_id = articles.article_id
