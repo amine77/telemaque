ff45c20a23405b26160120ec4d6133901e01fc3e
amine <amine@192.168.1.75>
2015-09-09 23:53:12 +0200

    Correction du bug sur la recherche iPhone

--- application/models/Articles_model.php
+++ application/models/Articles_model.php
@@ -163,12 +163,24 @@ class Articles_model extends CI_Model {
 
     //cette fonction effectue une recherche sur les labels des articles ainsi que leurs mots clés associés
     public function search($key) {
-        $sql = "SELECT DISTINCT articles.article_id, article_label, image_path
-            FROM images
-           RIGHT JOIN articles ON articles.image_id = images.image_id
-           RIGHT JOIN tags_articles ON tags_articles.article_id = articles.article_id
-           RIGHT JOIN tags ON tags.tag_id = tags_articles.tag_id
-           WHERE article_label LIKE '%$key%' OR tag_label like '%$key%'  ";
+//        $sql = "SELECT DISTINCT articles.article_id, article_label, image_path
+//            FROM images
+//           RIGHT JOIN articles ON articles.image_id = images.image_id
+//           RIGHT JOIN tags_articles ON tags_articles.article_id = articles.article_id
+//           RIGHT JOIN tags ON tags.tag_id = tags_articles.tag_id
+//           WHERE article_label LIKE '%$key%' OR tag_label like '%$key%'  ";
+        
+        $sql = "SELECT  articles.article_id, article_label, image_path
+                FROM images
+                RIGHT JOIN articles ON articles.image_id = images.image_id
+                RIGHT JOIN tags_articles ON tags_articles.article_id = articles.article_id
+                RIGHT JOIN tags ON tags.tag_id = tags_articles.tag_id
+                WHERE tag_label like '%$key%'
+                 UNION DISTINCT
+                SELECT  articles.article_id, article_label, image_path
+                 FROM images
+                RIGHT JOIN articles ON articles.image_id = images.image_id
+                WHERE article_label LIKE '%$key%' ";
         $query = $this->db->query($sql);
         return $query->result_array();
     }
